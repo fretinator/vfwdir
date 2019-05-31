d8332930dbc45fcbd009bad49d8974fe16fefb7f
Tom Dison <fretinator@smallvue.com>
2019-05-31 09:13:44 -0500

    Initial version of script
    Initial version with only minimal error checking. Expects to be in the same directory as the vfloppy files, hard-coded windows name of executables (for cygwin), remove .exe extensions if on Linux.

---
+++ vfwdir.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 20 ]
+then
+        echo "Usage vfwdir image directory"
+        exit 1
+fi
+
+#check image file exists
+if [ ! -f $1 ]
+then
+        echo "Paramter 1 must be the D88 image file"
+        exit 2
+fi
+
+#check if parameter 2 is a directory
+if [ ! -d $2 ]
+then
+        echo "Parameter 2 must be a directory"
+        exit 3
+fi
+
+cd $2
+files=`ls *`
+cd -
+
+for f in $files
+do
+        if [ ! -d $f ]
+        then
+                echo "Processing File $f"
+                ./vfwrite.exe $1 $f
+        fi
+done
+
+echo "Completed."
+
